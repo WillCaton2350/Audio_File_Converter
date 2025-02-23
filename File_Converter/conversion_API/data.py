ab0c513b79606f8ccca70252636007fe8520b743
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2025-02-23 01:01:11 -0500

    Update data.py

--- File_Converter/conversion_API/data.py
+++ File_Converter/conversion_API/data.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class states:
-    api_key:str = 'api_production_154b8c93db3571ddb087e3bd4b57b85b799704bba9051b59633592bd2796c3f1.67bab0d3d6cf59b2c9a65b62.67bab0e4a5fd01cc937cb1cc'
+    api_key:str = '000000000000000000000000000000000000000000000000'
     base_url:str = "https://api.freeconvert.com/v1"
-    upload_file_path:str = '/Users/administrator/Desktop/Projects/PyQt6Projects/File_Converter/conversion_API/mp3_files/Rain Man (feat. Hazel Mulan).mp3'
+    upload_file_path:str = '/Users/administrator/Desktop/Projects/PyQt6Projects/File_Converter/conversion_API/mp3_files/example.mp3'
